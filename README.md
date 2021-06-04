58b4aabe5ac6daffda5d7dc6b674eb998be4e88f
lukas <lukashermann28@gmail.com>
2021-06-04 10:36:08 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# WebGPU Renderer Extension for Vega
+
+WebGPU extension for Vega. This renderer is currently **work in progress and not ready for use**.
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import WebGPURenderer from './WebGPURenderer';
+
+export default {
+  renderer: WebGPURenderer,
+  headless: WebGPURenderer
+  //handler: WebGPUHandler
+};
