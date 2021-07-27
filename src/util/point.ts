703955f4abc3933a17407b3397e52041b74eccec
lukas <lukashermann28@gmail.com>
2021-07-27 13:33:54 -0400

    dom util

---
+++ src/util/point.ts
@@ -0,0 +1,5 @@
+// taken from vega-scenegraph
+export default function (event, el) {
+  const rect = el.getBoundingClientRect();
+  return [event.clientX - rect.left - (el.clientLeft || 0), event.clientY - rect.top - (el.clientTop || 0)];
+}
